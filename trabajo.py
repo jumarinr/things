5b7e884f6332bad262c49cf0f54835a624a1b304
jumarinr <31374735+jumarinr@users.noreply.github.com>
2017-08-29 23:31:45 -0500

    Create trabajo.py

---
+++ trabajo.py
@@ -0,0 +1,8 @@
+verga=eval(input())
+area1=verga[0][0]*verga[0][1]*0.5
+area2=verga[1][0]*verga[1][1]*0.5
+area3=verga[2][0]*verga[2][1]*0.5
+verga[0][2]=area1
+verga[1][2]=area2
+verga[2][2]=area3
+print(verga)
